dd23a9f0af361f77a5aafeef5c14919bb763c2d3
JoshuaYeoh <joshuayeohkeatkeong@gmail.com>
2024-08-21 01:53:26 +0800

    yeoh

--- Expansions/Standalone NPCs and Towns Pack/[CP] NPC Katrielle/assets/Maps/Compatibility/Isla/z_KatrielleRock.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="z_KatrielleRock" tilewidth="16" tileheight="16" tilecount="1" columns="1">
- <image source="z_KatrielleRock_spring.png" width="16" height="16"/>
-</tileset>
--- Expansions/Standalone NPCs and Towns Pack/[CP] NPC Katrielle/assets/Maps/z_townInterior_2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="z_townInterior_2" tilewidth="16" tileheight="16" tilecount="128" columns="32">
- <image source="townInterior_2.png" width="512" height="64"/>
-</tileset>
